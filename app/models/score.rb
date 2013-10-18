1f78dea145cd1db37eddf3eba4e05011de7712cf
David Evans <davidjsevans@gmail.com>
2013-10-18 14:55:02 +1100

    update score model

--- app/models/score.rb
+++ app/models/score.rb
@@ -2,6 +2,6 @@ class Score < ActiveRecord::Base
   default_scope order("points DESC, time ASC")
   default_scope limit(3)
 
-  validates :name, :points, presence: true
+  validates :name, :points, :time, presence: true
   validates :points, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
 end
